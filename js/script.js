6e34a0682b2c8999ef875886212e89c5218f0a36
di-112 <73027626+di-112@users.noreply.github.com>
2021-02-14 15:38:53 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('.header__burger').click( function(event) {
+		$('.header__burger,.menu__list').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('.list__item').click( function(event) {
+		if(screen.width <= 700)$('.header__burger,.menu__list').removeClass('active');
+	});
+	$(".menu__list").on("click","a", function (event) {
+		event.preventDefault();
+		let id  = $(this).attr('href'),
+			 top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 500);
+  });
+});
+
