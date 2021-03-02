adccfcb8e470ad4cc625b6450216e96bdec3744c
di-112 <73027626+di-112@users.noreply.github.com>
2021-03-02 20:46:51 +0300

    added scss, upadated burger-menu

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 		$('.header__burger,.menu__list').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
-	$('.list__item').click( function(event) {
-		if(screen.width <= 700)$('.header__burger,.menu__list').removeClass('active');
+	$('.menu__item').click( function(event) {
+		if(screen.width <= 760)$('.header__burger,.menu__list').removeClass('active');
 	});
 	$(".menu__list").on("click","a", function (event) {
 		event.preventDefault();
